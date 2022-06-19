d7cdc78eb4b50fcae8bce2ce509f85dadb64bb12
levreen <leviocampo2020@gmail.com>
2022-06-19 17:50:05 +0800

    updated faq to use template

--- lenslocked/main.go
+++ lenslocked/main.go
@@ -36,8 +36,8 @@ func contactHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func faqHandler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	fmt.Fprint(w, "<h1>FAQ Page</h1>")
+	tplPath := filepath.Join("templates", "faq.gohtml")
+	executeTemplate(w, tplPath)
 }
 
 func main() {
