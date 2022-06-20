e1fcd8de1d0a3dc181b2ca2027292ff7f517ee4d
levreen <leviocampo2020@gmail.com>
2022-06-20 15:34:45 +0800

    update average2 using Variadic fuction and ellipis ... to pass an argument to non-slice function.

--- headfirstgo/average2/main.go
+++ headfirstgo/average2/main.go
@@ -8,16 +8,25 @@ import (
 	"strconv"
 )
 
-func main() {
+func average(num ...float64) { // This is float64 using Variadic Function *NOT* slice.
+	var sums float64
+	for _, val := range num {
+		sums += val
+	}
+	length := float64(len(num))
+	fmt.Printf("Average: %0.2f", sums/length)
+}
+
+func main() { // This uses slice of float64. But using ellipsis ... when passing sum as an argument works to the function average.
 	arguments := os.Args[1:]
-	var sum float64 = 0
+	var sum []float64
 	for _, argument := range arguments {
 		number, err := strconv.ParseFloat(argument, 64)
 		if err != nil {
 			log.Fatal(err)
 		}
-		sum += number
+		sum = append(sum, number)
+
 	}
-	sampleCount := float64(len(arguments))
-	fmt.Printf("Average: %0.2f\n", sum/sampleCount)
+	average(sum...)
 }
