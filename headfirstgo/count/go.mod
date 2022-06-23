c8ad489e1e376d238df5c04b54f5c38ac3bd50c2
levreen <leviocampo2020@gmail.com>
2022-06-23 19:25:51 +0800

    update to the datafile

---
+++ headfirstgo/count/go.mod
@@ -0,0 +1,5 @@
+module github.com/headfirstgo/count
+
+go 1.18
+
+require github.com/headfirstgo/datafile v0.0.0-20190307063009-c40c587e7269
--- headfirstgo/count/main.go
+++ headfirstgo/count/main.go
@@ -1,1 +1,22 @@
+// count tallies the number of times each line
+// occurs within a file.
+package main
 
+import (
+	"fmt"
+	"log"
+
+	"github.com/headfirstgo/datafile"
+)
+
+func main() {
+	lines, err := datafile.GetStrings("votes.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	counts := make(map[string]int)
+	for _, line := range lines {
+		counts[line]++
+	}
+	fmt.Println(counts)
+}
