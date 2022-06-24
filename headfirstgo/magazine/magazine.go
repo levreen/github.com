bc38db257e7b9989cb98e9dfef54837bdbcb7c24
levreen <leviocampo2020@gmail.com>
2022-06-24 17:21:16 +0800

    update to the magazine

--- headfirstgo/magazine/magazine.go
+++ headfirstgo/magazine/magazine.go
@@ -1,8 +1,24 @@
 package magazine
 
+// Address exported type
+type Address struct {
+	Street     string
+	City       string
+	State      string
+	PostalCode string
+}
+
 // Subscriber exported type
 type Subscriber struct {
-	Name   string
-	Rate   float64
-	Active bool
+	HomeAddress Address
+	Name        string
+	Rate        float64
+	Active      bool
+}
+
+// Employee exported type
+type Employee struct {
+	Name        string
+	Salary      float64
+	HomeAddress Address
 }
