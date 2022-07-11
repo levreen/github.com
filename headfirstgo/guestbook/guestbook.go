98f95b929cc051635d9677830dab11f2c32f5ef5
levreen <leviocampo2020@gmail.com>
2022-07-12 02:01:40 +0800

    update for some go files

--- headfirstgo/guestbook/guestbook.go
+++ headfirstgo/guestbook/guestbook.go
@@ -2,17 +2,21 @@ package main
 
 import (
 	"bufio"
+	"fmt"
 	"html/template"
 	"log"
 	"net/http"
 	"os"
 )
 
+// Guestbook is a struct used in rendering view.html
 type GuestBook struct {
 	SignatureCount int
 	Signatures     []string
 }
 
+// getStrings returns a slice of strings read from fileName, one
+// string per line.
 func getStrings(fileName string) []string {
 	var lines []string
 	file, err := os.Open(fileName)
@@ -29,12 +33,15 @@ func getStrings(fileName string) []string {
 	return lines
 }
 
+// check calls log.Fatal on any non-nil error.
 func check(err error) {
 	if err != nil {
 		log.Fatal(err)
 	}
 }
 
+// viewHandler reads guestbook signatures and displays them together
+// view a count of all signatures.
 func viewHandler(writer http.ResponseWriter, request *http.Request) {
 	signatures := getStrings("signatures.txt")
 	html, err := template.ParseFiles("view.html")
@@ -47,8 +54,31 @@ func viewHandler(writer http.ResponseWriter, request *http.Request) {
 	check(err)
 }
 
+// newHandler displays a form to enter a signature.
+func newHandler(writer http.ResponseWriter, request *http.Request) {
+	html, err := template.ParseFiles("new.html")
+	check(err)
+	err = html.Execute(writer, nil)
+	check(err)
+}
+
+// createHandler takes a POST request with a signature to add, and
+// appends it to the signatures file.
+func createHandler(writer http.ResponseWriter, request *http.Request) {
+	signature := request.FormValue("signature")
+	options := os.O_WRONLY | os.O_APPEND | os.O_CREATE
+	file, err := os.OpenFile("signatures.txt", options, os.FileMode(0600))
+	check(err)
+	_, err = fmt.Fprintln(file, signature)
+	err = file.Close()
+	check(err)
+	http.Redirect(writer, request, "/guestbook", http.StatusFound)
+}
+
 func main() {
 	http.HandleFunc("/guestbook", viewHandler)
+	http.HandleFunc("/guestbook/new", newHandler)
+	http.HandleFunc("/guestbook/create", createHandler)
 	err := http.ListenAndServe("localhost:8080", nil)
 	log.Fatal(err)
 }
